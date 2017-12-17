f9bb64d7aa7158e7c832827707f572655b66a541
sssnowyue <sssnowyue@163.com>
2017-12-17 21:22:23 +0800

    require

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
 # # -*- coding: utf-8 -*-
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
-
-with open(r'txt/sms.txt', 'rb') as f:
-    sms = pickle.load(f)
-
-print sms[0][4]
+print "hello!!!"
+print "hello world"
